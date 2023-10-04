899966f41d7826ba4c39dfb85578b9c5c0ffe495
Mateusz Gdula <m.gdula@macopedia.com>
2023-10-05 01:08:31 +0200

    5. Backend layout

---
+++ components/T3/T3BlTwoColumns.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+    <div class="w-3/4">
+      <T3Renderer
+        v-if="content?.colPos1"
+        :content="content.colPos1"
+      />
+    </div>
+    <div class="w-1/4">
+      <T3Renderer
+        v-if="content?.colPos0"
+        :content="content.colPos0"
+      />
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+import { T3BackendLayout } from '@t3headless/nuxt-typo3'
+defineProps<T3BackendLayout>()
+</script>
