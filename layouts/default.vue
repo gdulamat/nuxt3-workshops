ad41824ba8d07faae9bf481db15101a80d8a180f
Mateusz Gdula <m.gdula@macopedia.com>
2023-10-04 16:59:38 +0200

    2. Frontend layout added

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const initialData = useT3InitialDataState()
+const navigation = computed(() => initialData.value.navigation?.[0]?.children || [])
+</script>
+
+<template>
+  <div>
+    <nav class="flex py-2 justify-center border-orange-500 border-b-2 sticky top-0 bg-white">
+      <NuxtLink class="mx-2"  to="/">Main page</NuxtLink>
+      <NuxtLink class="mx-2" v-for="item in navigation" :to="item.link">{{ item.title }}</NuxtLink>
+    </nav>
+    <NuxtPage />
+  </div>
+</template>
