3fe7e4f6630bc2a4fd51c94148ee848528b4d9b7
Mateusz Gdula <m.gdula@macopedia.com>
2023-10-05 00:01:11 +0200

    3. Dynamic frontend layout

---
+++ pages/[...slug].vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout :name="frontendLayout">
+    <T3BackendLayout :name="backendLayout" :content="pageData?.content" />
+  </NuxtLayout>
+</template>
+
+<script setup>
+const { frontendLayout, backendLayout, pageData } = await useT3Page()
+definePageMeta({
+  layout: false
+})
+</script>
